ec3e472100f8dff74cf125cc62f0cf99e99de9c6
Khushali Vaidya <khushalivaidya0@gmail.com>
2023-12-15 18:20:05 +0530

    added spinner

--- src/Components/RepoGrid.jsx
+++ src/Components/RepoGrid.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useMemo, useContext } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import axios from "axios";
+import Alert from "@mui/material/Alert";
 import link_icon from "../Images/external-link.svg";
 
 const RepoGrid = () => {
@@ -13,21 +14,27 @@ const RepoGrid = () => {
     const [paginationSize, setPaginationSize] = useState(10);
     const [pageNumber, setPageNumber] = useState(1);
     const apiUrl = `https://api.github.com/search/repositories?q=created:2019-01-10&sort=stars&order=desc&per_page=${paginationSize}&page=${pageNumber}`;
-
-    async function fetchRepos() {
-        try {
-            const response = await axios.get(apiUrl);
-            setRows(response?.data?.items);
-        } catch {}
-    }
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         setPageNumber(1);
     }, [paginationSize]);
 
     useEffect(() => {
+        async function fetchRepos() {
+            try {
+                setIsLoading(true);
+                const response = await axios.get(apiUrl);
+                setRows(response?.data?.items);
+                setIsLoading(false);
+            } catch {
+                // setIsLoading(false);
+                setPageNumber(1);
+            }
+        }
+
         fetchRepos();
-    }, [pageNumber, paginationSize]);
+    }, [pageNumber, paginationSize, apiUrl]);
 
     const handlePrevious = () => {
         setPageNumber(pageNumber - 1);
@@ -44,7 +51,11 @@ const RepoGrid = () => {
                     <div className="pagination-left">
                         Page Size:{" "}
                         <div>
-                            <FormControl fullWidth size="small">
+                            <FormControl
+                                fullWidth
+                                size="small"
+                                disabled={isLoading}
+                            >
                                 <Select
                                     value={paginationSize}
                                     onChange={(event) =>
@@ -61,46 +72,61 @@ const RepoGrid = () => {
                     <div className="pagination-section-right">
                         <button
                             onClick={handlePrevious}
-                            disabled={pageNumber === 1}
+                            disabled={pageNumber === 1 || isLoading}
                             className="pagination-btn"
                         >
                             Previous Page
                         </button>
                         <div className="current-page">Page: {pageNumber}</div>
-                        <button onClick={handleNext} className="pagination-btn">
+                        <button
+                            onClick={handleNext}
+                            disabled={isLoading}
+                            className="pagination-btn"
+                        >
                             Next Page
                         </button>
                     </div>
                 </div>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th className="name-col">Name</th>
-                        <th className="owner-col">Owner</th>
-                        <th className="description-col-head">Description</th>
-                        <th className="star-col">Stars_count</th>
-                        <th className="url-col">URL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows.map((row) => (
+            {isLoading ? (
+                <div className="loading-spinner"></div>
+            ) : (
+                <table>
+                    <thead>
                         <tr>
-                            <td className="name-col">{row.name}</td>
-                            <td className="owner-col">{row.owner.login}</td>
-                            <td className="description-col">
-                                {row.description}
-                            </td>
-                            <td className="star-col">{row.stargazers_count}</td>
-                            <td className="url-col">
-                                <a href={row.html_url} target="blank">
-                                    <img src={link_icon} alt="Link to Repo" />
-                                </a>
-                            </td>
+                            <th className="name-col">Name</th>
+                            <th className="owner-col">Owner</th>
+                            <th className="description-col-head">
+                                Description
+                            </th>
+                            <th className="star-col">Stars_count</th>
+                            <th className="url-col">URL</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {rows.map((row) => (
+                            <tr>
+                                <td className="name-col">{row.name}</td>
+                                <td className="owner-col">{row.owner.login}</td>
+                                <td className="description-col">
+                                    {row.description}
+                                </td>
+                                <td className="star-col">
+                                    {row.stargazers_count}
+                                </td>
+                                <td className="url-col">
+                                    <a href={row.html_url} target="blank">
+                                        <img
+                                            src={link_icon}
+                                            alt="Link to Repo"
+                                        />
+                                    </a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
